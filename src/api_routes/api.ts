3c2fac7e450f64f068fcc4271ff66f7b2db20d0d
Michael Zhang <zhengmichael20@gmail.com>
2021-06-13 17:38:21 -0400

    added the post router to the api router

--- src/api_routes/api.ts
+++ src/api_routes/api.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import auth_router from "./auth_routes";
+import posts_router from "./posts_route";
 
 const api_router = express.Router({mergeParams: true});
 
 api_router.use("/auth", auth_router);
+api_router.use("/post", posts_router);
 
 export default api_router;
